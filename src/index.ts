39bd577de0c7dd4a4cc335da8473a1e08dcd1b2a
JoelcioFacanha <joelciomoreira@gmail.com>
2024-11-06 20:24:57 -0300

    Configurando Hot Reload com tsc-watch

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Bem vindo ao curso de nodejs.');
+    res.send('Bem vindo ao curso de nodejs - tsc-watch');
 });
 
 app.listen(3000, () => {
