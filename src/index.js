2d21e92795e74cab7033aac1be227b66011340fc
JoelcioFacanha <joelciomoreira@gmail.com>
2024-11-06 08:38:23 -0300

    Criando servidor nodejs com Express

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send('Bem vindo ao curso de nodejs.');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
