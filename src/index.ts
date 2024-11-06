28865df272e172ba2e64799c9ffc00bad4a9d68b
JoelcioFacanha <joelciomoreira@gmail.com>
2024-11-06 20:59:01 -0300

    Express e os métodos HTTP

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
 import express, { Request, Response } from 'express';
 
 var app = express();
+app.use(express.json());
+
+let usuarios = [
+    { nome: "Maria dos Santos", idade: 25 },
+    { nome: "João Medeiros", idade: 30 },
+    { nome: "Claudia Maia", idade: 45 },
+    { nome: "João Carlos Silva", idade: 18 },
+];
 
 app.get('/', (req, res) => {
     res.send('Bem vindo ao curso de nodejs - tsc-watch');
 });
 
 app.get('/users', (req: Request, res: Response) => {
-    let usuarios = [
-        { nome: "Maria dos Santos", idade: 25 },
-        { nome: "João Medeiros", idade: 30 },
-        { nome: "Claudia Maia", idade: 45 },
-        { nome: "João Carlos Silva", idade: 18 },
-    ];
     res.send(usuarios);
+});
+
+app.post('/users', (req: Request, res: Response) => {
+    let user = req.body;
+    usuarios.push(user);
+    res.send({ mesage: 'Usuário criado com sucesso!' });
 });
 
 app.listen(3000, () => {
