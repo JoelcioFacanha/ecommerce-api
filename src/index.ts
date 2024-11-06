8ecc79a199f6147c6013126f40ff7f70caf645ed
JoelcioFacanha <joelciomoreira@gmail.com>
2024-11-06 20:41:51 -0300

    Testando API com o Postman

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 var app = express();
 
 app.get('/', (req, res) => {
     res.send('Bem vindo ao curso de nodejs - tsc-watch');
+});
+
+app.get('/users', (req: Request, res: Response) => {
+    let usuarios = [
+        { nome: "Maria dos Santos", idade: 25 },
+        { nome: "João Medeiros", idade: 30 },
+        { nome: "Claudia Maia", idade: 45 },
+        { nome: "João Carlos Silva", idade: 18 },
+    ];
+    res.send(usuarios);
 });
 
 app.listen(3000, () => {
